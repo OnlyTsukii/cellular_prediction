8a098edb3f3a856e90895271db9ce8e053a80c0f
OnlyTsukii <1431297183@qq.com>
2025-03-24 11:42:41 +0800

    optimize code

--- collector/labels.py
+++ collector/labels.py
@@ -11,33 +11,25 @@ class LabelsCollector:
             config = json.load(file)
 
         self.server_ip = config["SERVER_IP"]
-        self.tcp_ul_port = config["TCP_UL_PORT"]
-        self.tcp_dl_port = config["TCP_DL_PORT"]
-        self.udp_port = config["UDP_PORT"]
+        self.tcp_port = config["TCP_PORT"]
 
         self.ul_bandwidth = math.nan
-        self.dl_bandwidth = math.nan
         self.rtt = math.nan
-        self.jitter = math.nan
-        self.loss = math.nan
+        self.throughput = math.nan
+        self.retry = math.nan
+        self.cwnd = math.nan
     
-    def run_iperf_test(self, direction, protocol="tcp"):
+    def run_iperf_test(self):
         """Run iperf3 test and continuously parse results."""
         cmd = [
             "stdbuf", "-oL",  # Disable output buffering
             "iperf3",
             "-c", self.server_ip,
-            "-p", str(self.udp_port if protocol == "udp" else self.tcp_ul_port if direction == 'up' else self.tcp_dl_port),
+            "-p", str(self.tcp_port),
             "-t", "0",  # Infinite duration
             "-i", "1",
         ]
-        
-        if protocol == "udp":
-            cmd.extend(["-u"])  # UDP mode with bandwidth limit
-        
-        if direction == "up":
-            cmd.append("-R")  # Reverse mode for uplink measurement
-
+    
         print(cmd)
 
         process = subprocess.Popen(
@@ -53,16 +45,14 @@ class LabelsCollector:
                 if not line:
                     break
 
-                if "Mbits/sec" in line:
-                    if protocol == 'udp':
-                        self.jitter = float(line.split('Mbits/sec')[1].split('ms')[0].strip())
-                        self.loss = float(line.split('ms')[1].split('(')[1].split('%')[0].strip())
-                    else:
-                        if direction == "up":
-                            self.ul_bandwidth = float(line.split('MBytes')[1].split('Mbits/sec')[0].strip())
-                        else:
-                            self.dl_bandwidth = float(line.split('MBytes')[1].split('Mbits/sec')[0].strip())
-                        
+                if "Mbits/sec" in line and "sender" not in line:
+                    combine_data = line.split(' ')
+                    combine_data = [s for s in combine_data if s != '']
+                    self.ul_bandwidth = float(combine_data[6])
+                    self.throughput = float(combine_data[4])
+                    self.retry = int(combine_data[8])
+                    self.cwnd = int(combine_data[9])
+
             except Exception as e:
                 print(f"Error: {str(e)}")
     
@@ -93,23 +83,9 @@ class LabelsCollector:
     def start_services(self):
         """Start all background services."""
         # Start bandwidth tests
-        # threading.Thread(
-        #     target=self.run_iperf_test, 
-        #     args=("up", "tcp"),
-        # ).start()
+        threading.Thread(target=self.run_iperf_test).start()
 
-        # threading.Thread(
-        #     target=self.run_iperf_test, 
-        #     args=("down", "tcp"),
-        # ).start()
-
-        # Start UDP test
-        threading.Thread(
-            target=self.run_iperf_test, 
-            args=("up", "udp"),
-        ).start()
-
-        # # Start ping monitor
+        # Start ping monitor
         threading.Thread(target=self.ping_monitor).start()
 
 def main():
@@ -123,11 +99,11 @@ def main():
     while True:
         try:
             # Print metrics to console
-            print(f"Uplink: {collector.ul_bandwidth:.2f} Mbps")
-            print(f"Downlink: {collector.dl_bandwidth:.2f} Mbps")
+            print(f"Uplink Bandwidth: {collector.ul_bandwidth:.2f} Mbits/sec")
+            print(f"Throughput: {collector.throughput:.2f} MBytes")
             print(f"RTT: {collector.rtt:.2f} ms")
-            print(f"Jitter: {collector.jitter:.2f} ms")
-            print(f"Loss: {collector.loss:.2f}%")
+            print(f"Retry: {collector.retry} times")
+            print(f"CongestionWindow: {collector.cwnd} KBytes")
             print(f"----------------------------------------------")
             
             time.sleep(1)
